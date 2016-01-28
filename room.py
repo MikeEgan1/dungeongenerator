3086badc238c40ece904094087b240f2eb408ed6
Mike <phegan@gmail.com>
2016-01-27 21:41:02 -0500

    Updated the code to do more of the dungeon creation

--- room.py
+++ room.py
@@ -42,6 +42,7 @@ def intersect(self, other):
 def make_map():
     rooms = []
     num_rooms = 0
+    player = {'x' : 0, 'y' : 0}
 
     for r in xrange(MAX_ROOMS):
         #random width and height
@@ -51,3 +52,54 @@ def make_map():
         x = libtcod.random_get_int(0, 0, MAP_WIDTH - w - 1)
         y = libtcod.random_get_int(0, 0, MAP_HEIGHT - h - 1)
 
+
+        #"Rect" class makes rectangles easier to work with
+        new_room = Rect(x, y, w, h)
+
+        #run through the other rooms and see if they intersect with this one
+        failed = False
+        for other_room in rooms:
+            if new_room.intersect(other_room):
+                failed = True
+                break
+
+
+        if not failed:
+            #this means there are no intersections, so this room is valid
+
+            #"paint" it to the map's tiles
+            create_room(new_room)
+
+            #center coordinates of new room, will be useful later
+            (new_x, new_y) = new_room.center()
+
+            #optional: print "room number" to see how the map drawing worked
+            #          we may have more than ten rooms, so print 'A' for the first room, 'B' for the next...
+            room_no = Object(new_x, new_y, chr(65+num_rooms), libtcod.white)
+            objects.insert(0, room_no) #draw early, so monsters are drawn on top`
+
+            if num_rooms == 0:
+                #this is the first room, where the player starts at
+                player.x = new_x
+                player.y = new_y
+
+        else:
+            #all rooms after the first:
+            #connect it to the previous room with a tunnel
+
+            #center coordinates of previous room
+            (prev_x, prev_y) = rooms[num_rooms-1].center()
+
+            #draw a coin (random number that is either 0 or 1)
+            if libtcod.random_get_int(0, 0, 1) == 1:
+                #first move horizontally, then vertically
+                create_h_tunnel(prev_x, new_x, prev_y)
+                create_v_tunnel(prev_y, new_y, new_x)
+            else:
+                #first move vertically, then horizontally
+                create_v_tunnel(prev_y, new_y, prev_x)
+                create_h_tunnel(prev_x, new_x, new_y)
+
+        #finally, append the new room to the list
+        rooms.append(new_room)
+        num_rooms += 1
