bf5f4e7628ce84a000de57e465336d849250c12d
Mike <phegan@gmail.com>
2016-01-27 22:47:23 -0500

    Added the intersect method and added the rooms to the room list.

--- create_map.py
+++ create_map.py
@@ -6,17 +6,28 @@ MAP_HEIGHT = 50
 MAP_WIDTH = 50
 ROOM_MAX_SIZE = 10
 ROOM_MIN_SIZE = 6
-MAX_ROOMS = 2
+MAX_ROOMS = 3
 
 def main():
     dungeon_map = [['_']*MAP_WIDTH]*MAP_HEIGHT
+    rooms = []
     width = random.randrange(ROOM_MIN_SIZE, ROOM_MAX_SIZE)
     height = random.randrange(ROOM_MIN_SIZE, ROOM_MAX_SIZE)
     xcorner = random.randrange(0, MAP_WIDTH - width - 1)
     ycorner = random.randrange(0, MAP_HEIGHT - height - 1)
 
-    new_room = Rect(xcorner, ycorner, width, height)
-    new_room.print_rect()
+    for r in xrange(MAX_ROOMS):
+        new_room = Rect(xcorner, ycorner, width, height)
+        # new_room.print_rect()
+
+        for other_room in rooms:
+            if new_room.intersect(other_room):
+                break
+
+            rooms.append(new_room)
+
+    for room in rooms:
+        room.print_rect()
 
 
 
--- rect.py
+++ rect.py
@@ -14,3 +14,8 @@ class Rect:
             for y in xrange(self.y2 - self.y1):
                 sys.stdout.write('X')
             sys.stdout.write('\n')
+
+    def intersect(self, other):
+        #returns true if this rectangle intersects with another one
+        return (self.x1 <= other.x2 and self.x2 >= other.x1 and
+                self.y1 <= other.y2 and self.y2 >= other.y1)
