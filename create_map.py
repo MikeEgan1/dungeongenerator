0d8e02096e9cab55335db4f52f3ace42288f2680
Mike <phegan@gmail.com>
2016-01-27 22:57:03 -0500

    Created a create_room to add to the dungeon, and added a print for the entire dungeon.

--- create_map.py
+++ create_map.py
@@ -1,5 +1,6 @@
 from rect import Rect
 from tile import Tile
+import sys
 import random
 
 MAP_HEIGHT = 50
@@ -7,6 +8,21 @@ MAP_WIDTH = 50
 ROOM_MAX_SIZE = 10
 ROOM_MIN_SIZE = 6
 MAX_ROOMS = 3
+
+
+def create_room(room, dungeon_map):
+
+    for x in xrange(room.x1 + 1, room.x2):
+        for y in xrange(room.y1 + 1, room.y2):
+            dungeon_map[x][y] = 'X'
+
+    return dungeon_map
+
+def print_dungeon_map(dungeon_map):
+    for x in xrange(MAP_WIDTH):
+        for y in xrange(MAP_HEIGHT):
+            sys.stdout.write(dungeon_map[x][y])
+        sys.stdout.write('\n')
 
 def main():
     dungeon_map = [['_']*MAP_WIDTH]*MAP_HEIGHT
@@ -27,14 +43,13 @@ def main():
                 if new_room.intersect(other_room):
                     break
 
+                create_room(new_room, dungeon_map)
                 rooms.append(new_room)
         else:
+            create_room(new_room, dungeon_map)
             rooms.append(new_room)
 
-    for room in rooms:
-        room.print_rect()
-
-
+    print_dungeon_map(dungeon_map)
 
 if __name__ == "__main__":
     main()
--- rect.py
+++ rect.py
@@ -9,13 +9,19 @@ class Rect:
         self.x2 = xcorner + width
         self.y2 = ycorner + height
 
+    def intersect(self, other):
+        #returns true if this rectangle intersects with another one
+        return (self.x1 <= other.x2 and self.x2 >= other.x1 and
+                self.y1 <= other.y2 and self.y2 >= other.y1)
+
+    def center(self):
+        center_x = (self.x1 + self.x2) / 2
+        center_y = (self.y1 + self.y2) / 2
+        return (center_x, center_y)
+
+
     def print_rect(self):
         for _ in xrange(self.x2 - self.x1):
             for _ in xrange(self.y2 - self.y1):
                 sys.stdout.write('X')
             sys.stdout.write('\n')
-
-    def intersect(self, other):
-        #returns true if this rectangle intersects with another one
-        return (self.x1 <= other.x2 and self.x2 >= other.x1 and
-                self.y1 <= other.y2 and self.y2 >= other.y1)
