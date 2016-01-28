0edceec6b102c369cca7e2efa80734e632b21541
Mike <phegan@gmail.com>
2016-01-27 21:28:42 -0500

    Adding the rect, room and tile code

--- room.py
+++ room.py
@@ -1,7 +1,6 @@
 from rect import Rect
 from tile import Tile
 
-
 MAP_HEIGHT = 200
 MAP_WIDTH = 200
 
@@ -12,6 +11,19 @@ def create_room(room):
         for y in xrange(room.y1, room.y2 + 1):
             map[x][y].blocked = False
             map[x][y].block_sight = False
+
+def create_h_tunnel(x1, x2, y):
+    global map
+    for x in xrange(min(x1, x2), max(x1, x2) + 1):
+        map[x][y].blocked = False
+        map[x][y].block_sight = False
+
+def create_v_tunnel(y1, y2, x):
+    global map
+    #vertical tunnel
+    for y in xrange(min(y1, y2), max(y1, y2) + 1):
+        map[x][y].blocked = False
+        map[x][y].block_sight = False
 
 def make_map():
     global map
@@ -24,5 +36,6 @@ def make_map():
     #create two rooms
     room1 = Rect(20, 15, 10, 15)
     room2 = Rect(50, 15, 10, 15)
+    create_h_tunnel(25, 55, 23)
     create_room(room1)
     create_room(room2)
